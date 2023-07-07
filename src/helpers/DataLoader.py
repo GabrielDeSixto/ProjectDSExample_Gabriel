04e32b3125be6e93d9f60a87b515ee32b4d71d91
gabriel.solano.de.sixto <gabrielsolanodesixto@gmail.com>
2023-07-06 23:29:17 -0600

    POO example

---
+++ src/helpers/DataLoader.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+class DataLoader:
+    def __init__(self, path):
+        self.path = path
+
+    def preprocedata(self, data):
+        pass
+
+
+def load_data(self):
+    data = pd.read_csv(self.path)
+    return data
+
+
+# void clase que no te retorna nada
